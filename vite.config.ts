69bbfcdb0b38cd5c576ee9e3df0b50c279c5c2fe
JmGuerrero20 <ju.ma.gue.ra@gmail.com>
2026-08-25 15:55:26 -0500

    Configure public Sites deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import react from '@vitejs/plugin-react'
+import { sites } from '@openai/sites-vite-plugin'
 import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), sites()],
 })
 
