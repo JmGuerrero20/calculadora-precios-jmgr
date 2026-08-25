f4eb52eafc7930a74322abcaa44b392407e42c82
JmGuerrero20 <ju.ma.gue.ra@gmail.com>
2026-08-25 16:00:26 -0500

    Add GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: process.env.GITHUB_ACTIONS ? '/calculadora-precios-jmgr/' : '/',
   plugins: [react(), sites()],
 })
 
